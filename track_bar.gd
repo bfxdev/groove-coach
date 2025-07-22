89e28b691eafc36d4dc9e5c5a2e6378fc8fa2b33
bfxdev <11587816+bfxdev@users.noreply.github.com>
2025-07-22 06:55:02 +0200

    Added fonts and docs, started swing UI

--- track_bar.gd
+++ track_bar.gd
@@ -1,5 +1,3 @@
-extends Node
-
 # https://www.quora.com/What-is-the-difference-between-4-4-time-and-2-2-time-and-how-does-this-affect-the-music
 # https://fr.wikipedia.org/wiki/Notes_in%C3%A9gales
 
